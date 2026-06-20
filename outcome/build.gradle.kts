a557a7ae62ad35d367bd04bf2668e7f2e9d29f49
badminimum <contact@badminimum.com>
2026-06-20 14:17:40 +0200

    bump outcome to 1.1.0

--- outcome/build.gradle.kts
+++ outcome/build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
 }
 
 publishingConvention {
-    version.set("1.0.0")
+    version.set("1.1.0")
     description.set("A functional wrapper for type-safe success and failure handling, supporting monadic chaining and error recovery.")
 }
